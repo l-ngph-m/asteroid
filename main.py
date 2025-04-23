a0a1023da4398abf149ed8a68df2aa53b1518ca9
Long <phamlogg@gmail.com>
2025-04-23 13:39:35 +0200

    second commit

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import pygame
+import sys
+from player import Player
+from circleshape import CircleShape
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from shot import Shot
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over")
+                sys.exit()
+        for thing in drawable:
+            thing.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick()/1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
